b4aad8f6123a7d9fc8ea57623e63cdfa135c02c9
nobrelli <ryuedwards@gmail.com>
2024-12-25 22:15:47 +0800

    Add Notifications component to App for user feedback

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,6 +1,8 @@
 import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
 import '@mantine/dates/styles.css'
+import '@mantine/notifications/styles.css'
+import { Notifications } from '@mantine/notifications'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Index from './screens/Index'
 import NewPatient from './screens/NewPatient'
@@ -18,6 +20,7 @@ export default function App() {
           <Route path="/settings" element={<Settings />} />
         </Routes>
       </BrowserRouter>
+      <Notifications limit={3} />
     </MantineProvider>
   )
 }
