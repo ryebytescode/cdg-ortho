8f6baf91dc6cd1f521fae95553b5308987846427
nobrelli <ryuedwards@gmail.com>
2024-12-28 09:40:45 +0800

    Enhance name formatting utility: add optional suffix parameter

--- src/renderer/src/helpers/utils.ts
+++ src/renderer/src/helpers/utils.ts
@@ -6,9 +6,10 @@ export function joinNames(
   first: string,
   last: string,
   mid?: string,
+  suffix?: string,
   isLastFirst = false
 ) {
   return isLastFirst
-    ? `${last}, ${first}${mid && ` ${mid?.at(0)}.`}`
-    : `${first}${mid && ` ${mid?.at(0)}.`} ${last}`
+    ? `${last}${suffix ? ` ${suffix}` : ''}, ${first}${mid ? ` ${mid?.at(0)}.` : ''}`
+    : `${first}${mid ? ` ${mid?.at(0)}.` : ''} ${last}${suffix ? ` ${suffix}` : ''}`
 }
