ef15ae9efcfafb9a4b34b490f032a390d3a3e10f
Ryu Rabino <ryebytescode@proton.me>
2025-01-01 15:34:57 +0800

    Add new environment variables

--- src/shared/types/env.d.ts
+++ src/shared/types/env.d.ts
@@ -6,6 +6,9 @@ declare global {
       MAIN_VITE_TURSO_AUTH_TOKEN: string
       MAIN_VITE_CUID2_FINGERPRINT: string
       MAIN_VITE_DB_ENCRYPTION_KEY: string
+      MAIN_VITE_SETTINGS_FILE_NAME: string
+      MAIN_VITE_TEMP_FOLDER: string
+      MAIN_VITE_UPLOADS_FOLDER: string
     }
   }
 }
