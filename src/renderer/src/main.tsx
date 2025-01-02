28473a04703e55a3594ec924bcb59a27c0d11194
Ryu Rabino <ryebytescode@proton.me>
2025-01-02 13:13:13 +0800

    Removed StrictMode

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -8,7 +8,5 @@ if (typeof window !== 'undefined') {
 }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 )
