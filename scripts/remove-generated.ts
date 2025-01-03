701e9f54db5ca17e08d3b8788ee14c928582a47b
Ryu Rabino <ryebytescode@proton.me>
2025-01-03 15:08:17 +0800

    Add script to remove generated files and directories

---
+++ scripts/remove-generated.ts
@@ -0,0 +1,21 @@
+import * as fs from 'node:fs/promises'
+import * as path from 'node:path'
+import { DB_FILE_NAME } from '../src/shared/constants'
+
+;(async () => {
+  const toDelete = [
+    path.join(__dirname, '..', 'migrations'),
+    path.join(__dirname, '..', 'dist'),
+    path.join(__dirname, '..', `build/${DB_FILE_NAME}.db`),
+  ]
+
+  for (const entry of toDelete) {
+    try {
+      await fs.access(entry)
+      await fs.rm(entry, { recursive: true, force: true })
+      console.log(`${entry} deleted.`)
+    } catch {
+      console.log(`${entry} does not exist, skipped.`)
+    }
+  }
+})()
