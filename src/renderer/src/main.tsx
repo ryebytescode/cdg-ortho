479793f084ac2518df6babce592debd1c6285658
Ryu Rabino <ryebytescode@proton.me>
2025-01-03 15:08:39 +0800

    Remove unused imports

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,11 +1,5 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { scan } from 'react-scan'
 import App from './App'
-
-if (typeof window !== 'undefined') {
-  // scan({ enabled: true })
-}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
