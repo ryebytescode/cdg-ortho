df326bf34b3b6979accdc17dc5bdf69c817d4104
Ryu Rabino <ryebytescode@proton.me>
2025-01-04 13:46:43 +0800

    Refactor logger initialization

--- src/main/logger.ts
+++ src/main/logger.ts
@@ -1,18 +1,14 @@
-import path from 'node:path'
+import path, { dirname } from 'node:path'
+import { app } from 'electron'
 import log from 'electron-log/main'
-import { APP_FOLDER, LOGS_FOLDER } from '../shared/types/constants'
-import { getSettingsSync } from './utils'
+import { LOGS_FOLDER } from '../shared/constants'
 
 export function initializeLogger() {
   log.initialize()
-
-  const settings = getSettingsSync()
-
   log.transports.file.maxSize = 5242880 // 5 mb
   log.transports.file.resolvePathFn = (vars) =>
     path.join(
-      settings.appDataFolder,
-      APP_FOLDER,
+      dirname(app.getPath('exe')),
       LOGS_FOLDER,
       // biome-ignore lint/style/noNonNullAssertion:
       vars.fileName!
