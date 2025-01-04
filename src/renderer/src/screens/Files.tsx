dc3f860829bea4714de62d1689ece7af9943d45e
Ryu Rabino <ryebytescode@proton.me>
2025-01-04 13:51:06 +0800

    Refactor file display logic to use Card for document types and improve thumbnail handling

--- src/renderer/src/screens/Files.tsx
+++ src/renderer/src/screens/Files.tsx
@@ -1,4 +1,5 @@
 import {
+  Card,
   Group,
   Image as MTImage,
   Overlay,
@@ -17,10 +18,15 @@ import {
   PDF_MIME_TYPE,
 } from '@mantine/dropzone'
 import { notifications } from '@mantine/notifications'
+import { FileIcon } from '@renderer/components/FileIcon'
 import { LoadingOverlay } from '@renderer/components/LoadingOverlay'
 import { PageView } from '@renderer/components/PageView'
-import { capFirstLetter, generateThumbnail } from '@renderer/helpers/utils'
-import type { FileCategory } from '@shared/constants'
+import {
+  capFirstLetter,
+  generateThumbnail,
+  getThumbnailUrl,
+} from '@renderer/helpers/utils'
+import { FileCategory, documentTypeMap } from '@shared/constants'
 import { IconPhoto, IconUpload, IconX } from '@tabler/icons-react'
 import { type ComponentRef, useEffect, useRef, useState } from 'react'
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
@@ -29,7 +35,7 @@ import { useParams } from 'react-router'
 const mimeTypes: Record<FileCategory, DropzoneProps['accept']> = {
   photos: ['image/jpeg', 'image/png'],
   videos: ['video/mp4', 'video/mpeg', 'video/avi'],
-  docs: MS_WORD_MIME_TYPE || PDF_MIME_TYPE || MIME_TYPES.csv,
+  docs: [...MS_WORD_MIME_TYPE, ...PDF_MIME_TYPE, MIME_TYPES.csv],
 }
 
 const CHUNK_SIZE = 1_048_576 // 1 MB
@@ -47,30 +53,54 @@ export default function PhotoManager({ category }: { category: FileCategory }) {
 
   useEffect(() => {
     const fetchFiles = async () => {
-      const files = await window.api.getFiles(patientId as string, category)
+      const files = await window.api.getFilesInfo(patientId as string, category)
       const newPreviews = files.map((file) => ({
         file: { ...file, path: '', id: '', patientId: '' },
-        thumbnail: file.thumbnail as string,
+        thumbnail: getThumbnailUrl(patientId as string, category, file.name),
       }))
       setPreviews(newPreviews)
-      console.log(newPreviews)
     }
 
     fetchFiles()
+  }, [patientId, category])
 
-    const handleProgressUpdate = (fileName: string, progress: number) => {
+  useEffect(() => {
+    const handleUploadComplete = (_, fileName: string) => {
       setUploadProgress((prev) => ({
         ...prev,
-        [fileName]: progress,
+        [fileName]: 100,
       }))
+
+      notifications.show({
+        title: 'Upload complete',
+        message: `File ${fileName} uploaded successfully`,
+        color: 'green',
+      })
+
+      loadingOverlayRef.current?.hide()
     }
 
-    window.api.onUploadProgress(handleProgressUpdate)
+    const handleUploadError = (_, fileName: string) => {
+      notifications.show({
+        title: 'Upload error',
+        message: `Failed to upload file ${fileName}`,
+        color: 'red',
+      })
+
+      loadingOverlayRef.current?.hide()
+    }
+
+    window.electron.ipcRenderer.on('upload-complete', handleUploadComplete)
+    window.electron.ipcRenderer.on('upload-error', handleUploadError)
 
     return () => {
-      window.api.removeUploadProgressListener(handleProgressUpdate)
+      window.electron.ipcRenderer.removeAllListeners('upload-complete')
+      window.electron.ipcRenderer.removeAllListeners('upload-error')
+
+      setPreviews([])
+      setUploadProgress({})
     }
-  }, [patientId, category])
+  }, [])
 
   const handleDrop = async (files: FileWithPath[]) => {
     loadingOverlayRef.current?.show()
@@ -82,8 +112,8 @@ export default function PhotoManager({ category }: { category: FileCategory }) {
       })
     )
 
-    setPreviews((prev) => [...prev, ...newPreviews])
-    loadingOverlayRef.current?.hide()
+    setPreviews((prev) => [...newPreviews, ...prev])
+    // loadingOverlayRef.current?.hide()
 
     files.map(async (file) => {
       const totalChunks = Math.ceil(file.size / CHUNK_SIZE)
@@ -102,7 +132,10 @@ export default function PhotoManager({ category }: { category: FileCategory }) {
           },
           name: file.name,
           size: file.size,
-          thumbnail: await generateThumbnail(file),
+          thumbnail:
+            file.category !== FileCategory.DOCS
+              ? await generateThumbnail(file)
+              : undefined,
         })
 
         startByte = endByte
@@ -113,12 +146,6 @@ export default function PhotoManager({ category }: { category: FileCategory }) {
           [file.name]: Math.round((i / totalChunks) * 100),
         }))
       }
-
-      notifications.show({
-        title: 'Upload complete',
-        message: `File ${file.name} uploaded successfully`,
-        color: 'green',
-      })
     })
   }
 
@@ -200,33 +227,42 @@ export default function PhotoManager({ category }: { category: FileCategory }) {
       <Paper mt="md">
         <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
           <Masonry gutter={18} sequential={true}>
-            {previews.map(({ file, thumbnail }) => (
-              <Paper
-                key={file.name}
-                style={{
-                  position: 'relative',
-                  overflow: 'hidden',
-                }}
-                radius="sm"
-              >
-                <MTImage
-                  src={thumbnail}
-                  alt={file.name}
-                  width="100%"
-                  onLoad={() => URL.revokeObjectURL(thumbnail)}
-                />
-                {uploadProgress[file.name] < 100 && (
-                  <Overlay backgroundOpacity={0.6} zIndex={5} blur={3} center>
-                    <Progress
-                      value={uploadProgress[file.name]}
-                      size="xl"
-                      transitionDuration={500}
-                      w="90%"
-                    />
-                  </Overlay>
-                )}
-              </Paper>
-            ))}
+            {previews.map(({ file, thumbnail }) =>
+              category === FileCategory.DOCS ? (
+                <Card key={file.name} withBorder w="100%">
+                  <FileIcon
+                    type={documentTypeMap[file.name.split('.').pop() as string]}
+                    size={32}
+                  />
+                  <Text>{file.name}</Text>
+                </Card>
+              ) : (
+                <Paper
+                  key={file.name}
+                  style={{
+                    position: 'relative',
+                    overflow: 'hidden',
+                  }}
+                  radius="sm"
+                >
+                  <MTImage
+                    src={thumbnail}
+                    alt={file.name}
+                    onLoad={() => URL.revokeObjectURL(thumbnail)}
+                  />
+                  {uploadProgress[file.name] < 100 && (
+                    <Overlay backgroundOpacity={0.6} zIndex={5} blur={3} center>
+                      <Progress
+                        value={uploadProgress[file.name]}
+                        size="xl"
+                        transitionDuration={500}
+                        w="90%"
+                      />
+                    </Overlay>
+                  )}
+                </Paper>
+              )
+            )}
           </Masonry>
         </ResponsiveMasonry>
       </Paper>
