1386868b6a2da1043b76e15923e9706bf2845e5b
Ryu Rabino <ryebytescode@proton.me>
2025-01-17 12:32:43 +0800

    fix: move database from resources into app data folder

--- src/main/database/connection.ts
+++ src/main/database/connection.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@libsql/client'
 import 'dotenv/config'
+import fs from 'node:fs'
 import path from 'node:path'
 import { drizzle } from 'drizzle-orm/libsql'
 import { app } from 'electron'
@@ -10,8 +11,22 @@ export function initializeDatabase() {
   const databaseFolder = !app.isPackaged
     ? path.join(app.getAppPath(), BUILD_FOLDER)
     : process.resourcesPath
+  const dbFile = path.join(databaseFolder, `${DB_FILE_NAME}.db`)
+  let dbURL = `file:${dbFile}`
+
+  if (fs.existsSync(dbFile) && app.isPackaged) {
+    const newDbFile = path.join(app.getPath('userData'), `${DB_FILE_NAME}.db`)
+
+    fs.cpSync(dbFile, newDbFile, {
+      force: true,
+      preserveTimestamps: true,
+    })
+    fs.rmSync(dbFile, { force: true })
+    dbURL = `file:${newDbFile}`
+  }
+
   const databaseClient = createClient({
-    url: `file:${path.join(databaseFolder, DB_FILE_NAME)}.db`,
+    url: dbURL,
   })
 
   return drizzle(databaseClient, { schema })
